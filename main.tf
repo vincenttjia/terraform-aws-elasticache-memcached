da0c491193873476e053dbb1745e0b0297a0ecbe
Nur Adhi N <nur.nugroho@traveloka.com>
2020-09-03 11:58:26 +0700

    Support Terraform v0.12 (#11)
    Signed-off-by: Nur Adhi Nugroho <nur.nugroho@traveloka.com>

--- main.tf
+++ main.tf
@@ -1,43 +1,54 @@
 locals {
   engine              = "memcached"
   cluster_id_format   = "%s-%s-"
-  mem_prefix          = "${format(local.cluster_id_format, var.service_name, "mem")}"
+  mem_prefix          = format(local.cluster_id_format, var.service_name, "mem")
   mem_max_name_length = "20"
-  mem_max_byte_length = "${(local.mem_max_name_length - length(local.mem_prefix)) / 2}"
-  mem_byte_length     = "${min(local.mem_max_name_length, local.mem_max_byte_length)}"
+  mem_max_byte_length = local.mem_max_name_length - length(local.mem_prefix) / 2
+  mem_byte_length     = min(local.mem_max_name_length, local.mem_max_byte_length)
 }
 
 resource "random_id" "cluster_id" {
-  prefix      = "${local.mem_prefix}"
-  byte_length = "${local.mem_max_byte_length}"
+  prefix      = local.mem_prefix
+  byte_length = local.mem_max_byte_length
 }
 
 resource "aws_elasticache_cluster" "this" {
-  cluster_id           = "${random_id.cluster_id.hex}"
-  engine               = "${local.engine}"
-  engine_version       = "${var.engine_version}"
-  node_type            = "${var.node_type}"
-  num_cache_nodes      = "${var.num_cache_nodes}"
-  port                 = "${var.port}"
-  apply_immediately    = "${var.apply_immediately}"
-  az_mode              = "${var.az_mode}"
-  parameter_group_name = "${var.parameter_group_name}"
-  subnet_group_name    = "${var.subnet_group_name}"
-  maintenance_window   = "${var.maintenance_window}"
+  cluster_id           = random_id.cluster_id.hex
+  engine               = local.engine
+  engine_version       = var.engine_version
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  port                 = var.port
+  apply_immediately    = var.apply_immediately
+  az_mode              = var.az_mode
+  parameter_group_name = var.parameter_group_name
+  subnet_group_name    = var.subnet_group_name
+  maintenance_window   = var.maintenance_window
 
-  security_group_ids = [
-    "${var.security_group_ids}",
-  ]
+  security_group_ids = var.security_group_ids
 
-  notification_topic_arn = "${var.notification_topic_arn}"
+  notification_topic_arn = var.notification_topic_arn
 
-  tags = "${merge(
+  tags = merge(
     var.additional_tags,
-    map("Name", random_id.cluster_id.hex),
-    map("Environment", var.environment),
-    map("ProductDomain", var.product_domain),
-    map("Service", var.service_name),
-    map("Description", var.description),
-    map("ManagedBy", "terraform")
-  )}"
+    {
+      "Name" = random_id.cluster_id.hex
+    },
+    {
+      "Environment" = var.environment
+    },
+    {
+      "ProductDomain" = var.product_domain
+    },
+    {
+      "Service" = var.service_name
+    },
+    {
+      "Description" = var.description
+    },
+    {
+      "ManagedBy" = "terraform"
+    },
+  )
 }
+
--- variables.tf
+++ variables.tf
@@ -1,84 +1,85 @@
 variable "product_domain" {
   description = "Product domain abbreviation this memcached belongs to"
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Environment of this memcached resides in"
-  type        = "string"
+  type        = string
 }
 
 variable "description" {
   description = "Description of what this memcached used for"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "service_name" {
   description = "Name of the service this memcached belongs to"
-  type        = "string"
+  type        = string
 }
 
 variable "maintenance_window" {
   description = "Preferred maintenance window for this memcached"
-  type        = "string"
+  type        = string
 }
 
 variable "node_type" {
   description = "Node type for this memcached"
-  type        = "string"
+  type        = string
 }
 
 variable "num_cache_nodes" {
   description = "Number of cache nodes for this memcached"
-  type        = "string"
+  type        = string
 }
 
 variable "parameter_group_name" {
   description = "Name of the parameter group used by this memcached cluster"
-  type        = "string"
+  type        = string
 }
 
 variable "security_group_ids" {
   description = "IDs of security groups to be attached to this memcached cluster"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "apply_immediately" {
   description = "Whether to apply cluster changes immediately, or during maintenance window"
-  type        = "string"
+  type        = string
 }
 
 variable "engine_version" {
   description = "Version of memcached engine for this cluster"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "subnet_group_name" {
   description = "Name of the subnet group this cluster resides in"
-  type        = "string"
+  type        = string
 }
 
 variable "notification_topic_arn" {
   description = "ARN of the SNS topic that should receive notification about this memcached cluster"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "port" {
   description = "Port used for this memcached"
-  type        = "string"
+  type        = string
   default     = "11211"
 }
 
 variable "az_mode" {
   description = "Whether to provision in single-az or cross-az"
-  type        = "string"
+  type        = string
 }
 
 variable "additional_tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Common additional tags"
 }
+
